a739a3d25044e522e74e3a2b99ce8b686e920d53
ajaould <aould@outlook.com>
2020-12-10 14:59:01 -0500

    items questions

--- game.js
+++ game.js
@@ -1,3 +1,3 @@
 function basicPopup(url) {
-    popupWindow = window.open(url,'popUpWindow','height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+    popupWindow = window.open(url,'popUpWindow','height=300,width=700,left=50,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
         }
